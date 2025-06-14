13b7d53a9ad861f53b7094e8b66deb5ad99632ac
Carlos Tolentino <carlost@gopassi.com>
2025-06-14 16:28:30 -0400

    Add logic to infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,10 +2,33 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "auth_lambda" {
-  source            = "./modules/lambda"
-  name              = "auth-service"
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "central-api-${var.env}"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+module "auth_login" {
+  source            = "./modules/auth-login"
+  name              = "auth-login"
   env               = var.env
   s3_bucket         = var.s3_bucket
-  source_code_hash  = var.source_code_hash
+  source_code_hash  = var.source_code_hash_auth_login
+  api_gateway_id    = aws_apigatewayv2_api.http_api.id
+  api_execution_arn = aws_apigatewayv2_api.http_api.execution_arn
+}
+
+module "auth_register" {
+  source            = "./modules/auth-register"
+  name              = "auth-register"
+  env               = var.env
+  s3_bucket         = var.s3_bucket
+  source_code_hash  = var.source_code_hash_auth_register
+  api_gateway_id    = aws_apigatewayv2_api.http_api.id
+  api_execution_arn = aws_apigatewayv2_api.http_api.execution_arn
 }
---
+++ infrastructure/modules/auth-login/variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "s3_bucket" {
+  type = string
+}
+
+variable "source_code_hash" {
+  type    = string
+  default = ""
+}
+
+variable "api_gateway_id" {
+  type        = string
+  description = "ID of the shared API Gateway"
+}
+
+variable "api_execution_arn" {
+  type        = string
+  description = "Execution ARN of the shared API Gateway"
+}
