1f9883602ef25898023ffabe421d0717502eb1e6
Carlos Tolentino <carlost@gopassi.com>
2025-06-14 11:38:22 -0400

    Add Api Gateway Terraform

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -25,3 +25,40 @@ resource "aws_lambda_function" "this" {
   source_code_hash = var.source_code_hash
   publish          = true
 }
+
+resource "aws_apigatewayv2_api" "this" {
+  name          = "${var.name}-${var.env}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "this" {
+  api_id                 = aws_apigatewayv2_api.this.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.this.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "this" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
+
+output "lambda_api_url" {
+  value = aws_apigatewayv2_api.this.api_endpoint
+}
